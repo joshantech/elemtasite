203b524ebc498956f379580173c1034e20bcbf3c
Joshan Christie <joshantech@gmail.com>
2026-01-13 16:41:55 -0500

    changed homepage sticky scroll

--- app/components/ui/sticky-scroll.tsx
+++ app/components/ui/sticky-scroll.tsx
@@ -32,35 +32,36 @@ const Component = forwardRef<HTMLDivElement>((props, ref) => {
             <div className='grid gap-2 col-span-4 left-column'>
               <figure className=' w-full'>
                 <img
-                  src='https://images.unsplash.com/photo-1718838541476-d04e71caa347?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/StickyNote.png'
                   alt=''
                   className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
                 />
               </figure>
               <figure className=' w-full'>
                 <img
-                  src='https://images.unsplash.com/photo-1715432362539-6ab2ab480db2?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/blanket.webp'
                   alt=''
                   className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
                 />
               </figure>
               <figure className=' w-full'>
                 <img
-                  src='https://images.unsplash.com/photo-1718601980986-0ce75101d52d?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/elemta.webp'
+                  alt=''
+                  className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
+                  style={{ objectPosition: 'center 20%' }}
+                />
+              </figure>
+              <figure className='w-full'>
+                <img
+                  src='/images/sticky-scroll/elemtabag.png'
                   alt=''
                   className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
                 />
               </figure>
               <figure className='w-full'>
                 <img
-                  src='https://images.unsplash.com/photo-1685904042960-66242a0ac352?w=500&auto=format&fit=crop'
-                  alt=''
-                  className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
-                />
-              </figure>
-              <figure className='w-full'>
-                <img
-                  src='https://images.unsplash.com/photo-1719411182379-ffd97c1f7ebf?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/smarter.svg'
                   alt=''
                   className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
                 />
@@ -69,21 +70,21 @@ const Component = forwardRef<HTMLDivElement>((props, ref) => {
             <div className='col-span-4 gap-2 grid grid-rows-3 sticky-column' style={{ height: '100vh' }}>
               <figure className='w-full h-full '>
                 <img
-                  src='https://images.unsplash.com/photo-1718969604981-de826f44ce15?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/wall.webp'
                   alt=''
                   className='transition-all duration-300 h-full w-full  align-bottom object-cover rounded-md '
                 />
               </figure>
               <figure className='w-full h-full '>
                 <img
-                  src='https://images.unsplash.com/photo-1476180814856-a36609db0493?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/glass.webp'
                   alt=''
                   className='transition-all duration-300 h-full w-full align-bottom object-cover rounded-md '
                 />
               </figure>
               <figure className='w-full h-full '>
                 <img
-                  src='https://images.unsplash.com/photo-1595407660626-db35dcd16609?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/faster.svg'
                   alt=''
                   className='transition-all duration-300 h-full w-full  align-bottom object-cover rounded-md '
                 />
@@ -92,35 +93,35 @@ const Component = forwardRef<HTMLDivElement>((props, ref) => {
             <div className='grid gap-2 col-span-4 right-column'>
               <figure className='w-full'>
                 <img
-                  src='https://images.unsplash.com/photo-1719547907790-f661a88302c2?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/shop.webp'
                   alt=''
                   className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
                 />
               </figure>
               <figure className='w-full'>
                 <img
-                  src='https://images.unsplash.com/photo-1599054799131-4b09c73a63cf?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/shirt.webp'
                   alt=''
                   className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
                 />
               </figure>
               <figure className='w-full'>
                 <img
-                  src='https://images.unsplash.com/photo-1719963532023-01b573d1d584?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/book.webp'
                   alt=''
                   className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
                 />
               </figure>
               <figure className='w-full'>
                 <img
-                  src='https://images.unsplash.com/photo-1714328101501-3594de6cb80f?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/cup.webp'
                   alt=''
                   className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
                 />
               </figure>
               <figure className='w-full'>
                 <img
-                  src='https://images.unsplash.com/photo-1719554873571-0fd6bf322bb1?w=500&auto=format&fit=crop'
+                  src='/images/sticky-scroll/further.svg'
                   alt=''
                   className='transition-all duration-300 w-full h-96  align-bottom object-cover rounded-md '
                 />
